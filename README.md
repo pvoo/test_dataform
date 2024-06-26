6de2fdd9a669239193b2979eb7f8014725dad64b
Ashish Alex <aalex@jaguarlandrover.com>
2024-06-26 19:16:27 +0100

    chore: format file

---
+++ README.md
@@ -0,0 +1,17 @@
+## FAQ
+
+1. I installed dataform cli after installing the extension and it does not work now
+
+   -  Uninstall the extension and re-install it. As currently it does not complete the registration of commands/disposables when a cli is missing
+
+    ```bash
+
+    #uninstall
+    npm uninstall -g @dataform/cli
+
+    #install
+    npm i -g @dataform/cli@^3.0.0-beta
+
+    ```
+
+
---
+++ ci/sqlfluff_dataform/sqlfluff_formatter.py
@@ -0,0 +1,19 @@
+import sys
+sqlfluff_config_path = sys.argv[1]
+my_bad_query = sys.argv[2]
+
+def fix_query(sqlfluff_config_path, my_bad_query):
+    try:
+        import sqlfluff
+    except ImportError:
+        return "sqlfluff is not installed"
+    my_good_query = sqlfluff.fix(
+                        my_bad_query,
+                        dialect="bigquery",
+                        config_path=str(sqlfluff_config_path),
+                        )
+    return my_good_query
+
+print(fix_query(sqlfluff_config_path, my_bad_query)) # so that GoLang can read stdout
+
+
