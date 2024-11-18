536e32f892572148c4900ca4d368b3803f6cb6e6
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-11-18 17:53:43 +0000

    fix: doc string

--- includes/utils.js
+++ includes/utils.js
@@ -3,7 +3,6 @@ let someFloatingPointNumber = 9.4;
 
 /**
  * Get book description
- * @constructor
  * @param {string} title - The title of the book.
  * @param {string} author - The author of the book.
  */
