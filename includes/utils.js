8adba31787660a1ec9857236ad032130f2daa4e1
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-11-18 17:18:53 +0000

    feat: update basic testing repo

---
+++ includes/utils.js
@@ -0,0 +1,18 @@
+let someVariable = "createUniqueKey";
+let someFloatingPointNumber = 9.4;
+
+/**
+ * Get book description
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function getBookDescription(title, author) {
+    return `Title: ${title}, author: ${author}`;
+}
+
+module.exports = {
+    someVariable,
+    someFloatingPointNumber,
+    getBookDescription,
+};
